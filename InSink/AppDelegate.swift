1fb37a0e0f82462b100db31d9e8f3e57170dfed9
Ben Oeyen <ben.oeyen@gmail.com>
2016-05-06 21:21:03 +0200

    Initial Commit

---
+++ InSink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InSink
+//
+//  Created by Ben Oeyen on 06/05/16.
+//  Copyright © 2016 Ben Oeyen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ InSink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  InSink
+//
+//  Created by Ben Oeyen on 06/05/16.
+//  Copyright © 2016 Ben Oeyen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
