720aa4340432842e492961f8a4d1fdbb0ead7da6
Ben Oeyen <ben.oeyen@gmail.com>
2017-03-30 09:30:33 +0200

    Added import/export of config

--- InSink/ConfigurationPopUpController.swift
+++ InSink/ConfigurationPopUpController.swift
@@ -43,9 +43,68 @@ class ConfigurationPopUpController: NSViewController {
         self.view.window?.close()
     }
     
+    @IBAction func exportConfig(_ sender: AnyObject){
+        var exportedConfig = ""
+        if(userDefaults.string(forKey: "extensions") != nil){
+            exportedConfig = exportedConfig + userDefaults.string(forKey: "extensions")!
+        }
+        exportedConfig = exportedConfig + ";_;"
+        if(userDefaults.string(forKey: "projectRoot") != nil){
+            exportedConfig = exportedConfig +  userDefaults.string(forKey: "projectRoot")!
+        }
+        exportedConfig = exportedConfig + ";_;"
+        if(userDefaults.string(forKey: "modules") != nil){
+            exportedConfig = exportedConfig +  userDefaults.string(forKey: "modules")!
+        }
+        exportedConfig = exportedConfig + ";_;"
+        if(userDefaults.string(forKey: "excludedPaths") != nil){
+            exportedConfig = exportedConfig + userDefaults.string(forKey: "excludedPaths")!
+        }
+        
+        let alert = NSAlert()
+        alert.messageText = "Configuration String"
+        alert.addButton(withTitle: "Close")
+        alert.informativeText = exportedConfig;
+        alert.beginSheetModal(for: self.view.window!, completionHandler: nil )
+    }
+    
+    @IBAction func importConfig(_ sender: AnyObject){
+        let importedConfig = getStringFromDialog()
+        if(importedConfig != nil){
+            var configs = importedConfig!.components(separatedBy: ";_;")
+            if(configs.count == 4){
+                userDefaults.set(configs[0], forKey:"extensions")
+                userDefaults.set(configs[1], forKey:"projectRoot")
+                userDefaults.set(configs[2], forKey:"modules")
+                userDefaults.set(configs[3], forKey:"excludedPaths")
+                viewDidLoad()
+            }
+        }
+    }
+    
+    func getStringFromDialog() -> String? {
+        let dialog = NSAlert()
+        dialog.addButton(withTitle: "Import")  // 1st button
+        dialog.addButton(withTitle: "Cancel")  // 2nd button
+        dialog.messageText = "Import Config"
+        dialog.informativeText = "Enter Configuration String"
+        
+        let txt = NSTextField(frame: NSRect(x: 0, y: 0, width: 400, height: 200))
+        txt.stringValue = ""
+        
+        dialog.accessoryView = txt
+        let response: NSModalResponse = dialog.runModal()
+        
+        if (response == NSAlertFirstButtonReturn) {
+            return txt.stringValue
+        } else {
+            return nil
+        }
+    }
+    
     override func viewDidLoad() {
         if(userDefaults.string(forKey: "extensions") != nil){
-            self.extensions  = parseExtensionsInput(userDefaults.string(forKey: "extensions")!)
+            self.extensions = parseExtensionsInput(userDefaults.string(forKey: "extensions")!)
             extensionsTextField.stringValue = userDefaults.string(forKey: "extensions")!
         }
         if(userDefaults.string(forKey: "projectRoot") != nil){
